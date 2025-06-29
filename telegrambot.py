2fa949574390c50e7f69750743b69f9857fd4f60
Abdul-Quddus <abdulquddusinda@gmail.com>
2025-06-29 06:46:20 +0100

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -314,7 +314,7 @@ def main():
     application.add_handler(MessageHandler(filters.Document.PDF, handle_document))
     application.add_handler(CallbackQueryHandler(button_callback))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_unknown))
-    application.add_handler(error_handler)
+    application.add_error_handler(error_handler)
     
     logger.info("Starting PDF Q&A Telegram Bot...")
     application.run_polling(allowed_updates=Update.ALL_TYPES)
